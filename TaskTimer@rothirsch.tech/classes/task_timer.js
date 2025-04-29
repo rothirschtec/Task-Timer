90e8cae5a80601bbf424cb4269d11dd818605f01
Rene Zingerle <r9@kaja.rothirsch.tech>
2025-04-29 16:28:30 +0200

    Wider

--- TaskTimer@rothirsch.tech/classes/task_timer.js
+++ TaskTimer@rothirsch.tech/classes/task_timer.js
@@ -56,7 +56,6 @@ class TaskTimer extends PanelMenu.Button {
 
             /* Add class to menu for scoped CSS */
             this.menu.actor.add_style_class_name('tasktimer-popup');
-            this.menu.actor.set_width("600px");
 
             /* Create navigation controls and task container */
             this._createTaskSectionWithNavigation();
